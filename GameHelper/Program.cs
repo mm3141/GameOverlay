af3f06ff0eb04a4acb6bf304f403b20a98e54f39
Zaafar Ahmed <zaafar.tahir@gmail.com>
2020-06-01 18:08:48 -0400

    coroutine first commit

--- GameHelper/Program.cs
+++ GameHelper/Program.cs
@@ -5,6 +5,9 @@
 namespace GameHelper
 {
     using System;
+    using System.IO;
+    using System.Threading;
+    using Coroutine;
 
     /// <summary>
     /// Class executed when the application starts.
@@ -16,7 +19,22 @@ namespace GameHelper
         /// </summary>
         private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            AppDomain.CurrentDomain.UnhandledException += (sender, exceptionArgs) =>
+            {
+                var errorText = "Program exited with message:\n " + exceptionArgs.ExceptionObject;
+                File.AppendAllText("Error.log", $"{DateTime.Now:g} {errorText}\r\n{new string('-', 30)}\r\n");
+                Environment.Exit(1);
+            };
+
+            var lastTime = DateTime.Now;
+            var currTime = DateTime.Now;
+            while (true)
+            {
+                currTime = DateTime.Now;
+                CoroutineHandler.Tick((currTime - lastTime).TotalSeconds);
+                lastTime = currTime;
+                Thread.Sleep(1);
+            }
         }
     }
 }
