60af5bd3a88c796b3afdda56e7526c5acaceebd6
Zaafar <zaafar.tahir@gmail.com>
2021-03-01 03:30:33 -0500

    forgot to add the events file

---
+++ GameHelper/CoroutineEvents/GameHelperEvents.cs
@@ -0,0 +1,50 @@
+﻿// <copyright file="GameHelperEvents.cs" company="None">
+// Copyright (c) None. All rights reserved.
+// </copyright>
+
+namespace GameHelper.CoroutineEvents
+{
+    using Coroutine;
+
+    /// <summary>
+    /// Co-Routine events specific to the GameHelper.
+    /// </summary>
+    public static class GameHelperEvents
+    {
+        /// <summary>
+        /// To Update data every frame before rendering.
+        /// </summary>
+        internal static readonly Event PerFrameDataUpdate = new Event();
+
+        /// <summary>
+        /// To submit ImGui code for generating the UI.
+        /// </summary>
+        internal static readonly Event OnRender = new Event();
+
+        /// <summary>
+        /// Gets the event raised when GameProcess has opened a new game.
+        /// </summary>
+        internal static readonly Event OnOpened = new Event();
+
+        /// <summary>
+        /// Gets the event raised just before the GameProcess has closed the game.
+        /// </summary>
+        internal static readonly Event OnClose = new Event();
+
+        /// <summary>
+        /// Gets the event raised when the game has changed its size, position or both.
+        /// </summary>
+        internal static readonly Event OnMoved = new Event();
+
+        /// <summary>
+        /// Gets the event raised when the game Foreground property has changed.
+        /// </summary>
+        internal static readonly Event OnForegroundChanged = new Event();
+
+        /// <summary>
+        /// Gets the Game Helper closing event. The event is called whenever
+        /// all the settings have to be saved.
+        /// </summary>
+        internal static readonly Event TimeToSaveAllSettings = new Event();
+    }
+}
---
+++ GameHelper/CoroutineEvents/RemoteEvents.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="RemoteEvents.cs" company="None">
+// Copyright (c) None. All rights reserved.
+// </copyright>
+
+namespace GameHelper.CoroutineEvents
+{
+    using Coroutine;
+
+    /// <summary>
+    /// Co-Routine events specific to the game.
+    /// </summary>
+    public static class RemoteEvents
+    {
+        /// <summary>
+        /// Gets the Area Changed event. Area change is detected by
+        /// checking if the time spend on the loading screen is greater
+        /// than the last recorded time.
+        ///
+        /// NOTE:Game provides the value of time spend on the loading screen.
+        /// </summary>
+        public static readonly Event AreaChanged = new Event();
+
+        /// <summary>
+        /// Gets the Area Change Detection event. When Area Change happens many
+        /// classes want to gather new data from the new Area. Unfortunately, Some of
+        /// the those classes have inter-dependicies with each other.
+        /// This event is called before AreaChanged event, so this event can
+        /// be used to resolve some inter-dependicies. This is required since co-routine
+        /// doesn't guarantees a certian order in execution of functions. In the future,
+        /// if inter-dependicies gets out of control we might have to ditch co-routines
+        /// for AreaChange scenario. So, use this event if your class can independently
+        /// UpdateData, otherwise use AreaChanged. This class data is already loaded when
+        /// this event is called.
+        /// </summary>
+        internal static readonly Event AreaChangeDetected = new Event();
+
+        /// <summary>
+        /// Gets the Current State Changed event.
+        /// </summary>
+        internal static readonly Event StateChanged = new Event();
+    }
+}
