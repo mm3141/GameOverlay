f3f47cd4a21c591d5cea66c37f90b3ede6d6c9f9
Zaafar <zaafar.tahir@gmail.com>
2021-03-13 21:09:49 -0500

    added radar plugin and moved sample plugin to new folder.

---
+++ Radar/Radar.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="Radar.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Radar
+{
+    using GameHelper.Plugin;
+
+    /// <summary>
+    /// <see cref="Radar"/> plugin.
+    /// </summary>
+    public sealed class Radar : PCore<RadarSettings>
+    {
+        /// <inheritdoc/>
+        public override void DrawSettings()
+        {
+        }
+
+        /// <inheritdoc/>
+        public override void DrawUI()
+        {
+        }
+
+        /// <inheritdoc/>
+        public override void OnDisable()
+        {
+        }
+
+        /// <inheritdoc/>
+        public override void OnEnable()
+        {
+        }
+
+        /// <inheritdoc/>
+        public override void SaveSettings()
+        {
+        }
+    }
+}
---
+++ Radar/RadarSettings.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="RadarSettings.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Radar
+{
+    using GameHelper.Plugin;
+
+    /// <summary>
+    /// <see cref="Radar"/> plugin settings class.
+    /// </summary>
+    public sealed class RadarSettings : IPSettings
+    {
+    }
+}
