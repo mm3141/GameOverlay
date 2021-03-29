f70a5290f67a2b78be3ed337459e560d37366e35
Zaafar <zaafar.tahir@gmail.com>
2021-03-29 16:45:51 -0400

    minor changes for testing purpoese

---
+++ GameHelper/RemoteEnums/Rarity.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="Rarity.cs" company="None">
+// Copyright (c) None. All rights reserved.
+// </copyright>
+
+namespace GameHelper.RemoteEnums
+{
+    /// <summary>
+    /// Read Rarity.dat file for Rarity to integer mapping.
+    /// </summary>
+    public enum Rarity
+    {
+        /// <summary>
+        /// Unknown Item/Monster. This is not a real one.
+        /// </summary>
+        Unknown = -1,
+
+        /// <summary>
+        /// Normal Item/Monster.
+        /// </summary>
+        Normal,
+
+        /// <summary>
+        /// Magic Item/Monster.
+        /// </summary>
+        Magic,
+
+        /// <summary>
+        /// Rare Item/Monster.
+        /// </summary>
+        Rare,
+
+        /// <summary>
+        /// Unique Item/Monster.
+        /// </summary>
+        Unique,
+    }
+}
---
+++ GameOffsets/Objects/Components/ObjectMagicProperties.cs
@@ -0,0 +1,11 @@
+﻿namespace GameOffsets.Objects.Components
+{
+    using System.Runtime.InteropServices;
+
+    [StructLayout(LayoutKind.Explicit, Pack = 1)]
+    public struct ObjectMagicPropertiesOffsets
+    {
+        [FieldOffset(0x00)] public ComponentHeader Header;
+        [FieldOffset(0x9C)] public int Rarity;
+    }
+}
