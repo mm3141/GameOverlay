0bb50d3e8b0f5bb5360674fdcffc844039cdca45
Zaafar Ahmed <zaafar.tahir@gmail.com>
2021-07-27 20:03:08 -0400

    Revert "Added Player Inventory Ui Element Data reader."
    This reverts commit 034b8057a20512cf75eedf917e505e88def6e232.
    but keeps the QOL features.

--- GameOffsets/Objects/States/InGameState/ImportantUiElementsOffsets.cs
+++ GameOffsets/Objects/States/InGameState/ImportantUiElementsOffsets.cs
@@ -10,23 +10,13 @@ namespace GameOffsets.Objects.States.InGameState
     [StructLayout(LayoutKind.Explicit, Pack = 1)]
     public struct ImportantUiElementsOffsets
     {
-        [FieldOffset(0x540)] public InventoryAndStashPanelStruct InvAndStashPanels;
         [FieldOffset(0x648)] public IntPtr MapParentPtr;
     }
 
     [StructLayout(LayoutKind.Explicit, Pack = 1)]
     public struct MapParentStruct
     {
-        [FieldOffset(0x238)] public IntPtr LargeMapPtr; // MapUiElement struct
-        [FieldOffset(0x240)] public IntPtr MiniMapPtr; // MapUiElement struct
-    }
-
-    // not a real struct but they are always side by side.
-    // they are at UiRoot -> MainChild -> 35th, 36th Child respectively.
-    [StructLayout(LayoutKind.Explicit, Pack = 1)]
-    public struct InventoryAndStashPanelStruct
-    {
-        [FieldOffset(0x00)] public IntPtr InventoryPanelPtr; // InventoryPanelUiElement struct.
-        // [FieldOffset(0x08)] public IntPtr StashPanelPtr;
+        [FieldOffset(0x238)] public IntPtr LargeMapPtr;
+        [FieldOffset(0x240)] public IntPtr MiniMapPtr;
     }
 }
