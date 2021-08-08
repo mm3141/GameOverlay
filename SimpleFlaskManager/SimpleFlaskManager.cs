c50e12f1ade83b7defd7261aa0703d1274d05944
Zaafar Ahmed <zaafar.tahir@gmail.com>
2021-08-08 15:19:26 -0400

    added simpleflaskmanager plugin.

---
+++ SimpleFlaskManager/SimpleFlaskManager.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="SimpleFlaskManager.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace SimpleFlaskManager
+{
+    using GameHelper.Plugin;
+
+    /// <summary>
+    /// <see cref="SimpleFlaskManager"/> plugin.
+    /// </summary>
+    public sealed class SimpleFlaskManager : PCore<SimpleFlaskManagerSettings>
+    {
+        /// <inheritdoc/>
+        public override void DrawSettings()
+        {
+        }
+
+        /// <inheritdoc/>
+        public override void DrawUI()
+        {
+        }
+
+        /// <inheritdoc/>
+        public override void OnDisable()
+        {
+        }
+
+        /// <inheritdoc/>
+        public override void OnEnable(bool isGameOpened)
+        {
+        }
+
+        /// <inheritdoc/>
+        public override void SaveSettings()
+        {
+        }
+    }
+}
---
+++ SimpleFlaskManager/SimpleFlaskManagerSettings.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="SimpleFlaskManagerSettings.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace SimpleFlaskManager
+{
+    using GameHelper.Plugin;
+
+    /// <summary>
+    /// <see cref="SimpleFlaskManager"/> plugin settings class.
+    /// </summary>
+    public sealed class SimpleFlaskManagerSettings : IPSettings
+    {
+    }
+}
