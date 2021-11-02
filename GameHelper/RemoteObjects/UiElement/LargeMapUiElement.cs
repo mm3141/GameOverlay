93581733fee03018f7bfe6573a4ed7a804515c8e
instant.sc <code@insc.cc>
2021-11-02 23:07:57 +0300

    Unify MapElement code

--- GameHelper/RemoteObjects/UiElement/LargeMapUiElement.cs
+++ GameHelper/RemoteObjects/UiElement/LargeMapUiElement.cs
@@ -6,39 +6,20 @@ namespace GameHelper.RemoteObjects.UiElement
 {
     using System;
     using System.Numerics;
-    using GameOffsets.Objects.UiElement;
     using ImGuiNET;
 
     /// <summary>
-    ///     Points to the Map UiElement.
+    ///     Points to the LargeMap UiElement.
+    ///     It is exactly like any other element, except its in-memory position is its center
     /// </summary>
-    public class LargeMapUiElement : UiElementBase
+    public class LargeMapUiElement : MapUiElement
     {
-        private Vector2 defaultShift = Vector2.Zero;
-        private Vector2 shift = Vector2.Zero;
-
         /// <summary>
         ///     Initializes a new instance of the <see cref="LargeMapUiElement" /> class.
         /// </summary>
         /// <param name="address">address to the Map Ui Element of the game.</param>
         internal LargeMapUiElement(IntPtr address)
             : base(address) { }
-
-        /// <summary>
-        ///     Gets the value indicating how much map has shifted.
-        /// </summary>
-        public Vector2 Shift => this.shift;
-
-        /// <summary>
-        ///     Gets the value indicating shifted amount at rest (default).
-        /// </summary>
-        public Vector2 DefaultShift => this.defaultShift;
-
-        /// <summary>
-        ///     Gets the value indicating amount of zoom in the Map.
-        ///     Normally values are between 0.5f  - 1.5f.
-        /// </summary>
-        public float Zoom { get; private set; } = 0.5f;
 
         /// <inheritdoc />
         public override Vector2 Postion => Vector2.Zero;
@@ -58,33 +39,6 @@ namespace GameHelper.RemoteObjects.UiElement
         {
             base.ToImGui();
             ImGui.Text($"Center (without shift/default-shift) {this.Center}");
-            ImGui.Text($"Shift {this.shift}");
-            ImGui.Text($"Default Shift {this.defaultShift}");
-            ImGui.Text($"Zoom {this.Zoom}");
-        }
-
-        /// <inheritdoc />
-        protected override void CleanUpData()
-        {
-            base.CleanUpData();
-            this.shift = default;
-            this.defaultShift = default;
-            this.Zoom = 0.5f;
-        }
-
-        /// <inheritdoc />
-        protected override void UpdateData(bool hasAddressChanged)
-        {
-            base.UpdateData(hasAddressChanged);
-            var reader = Core.Process.Handle;
-            var data = reader.ReadMemory<MapUiElementOffset>(this.Address);
-            this.shift.X = data.Shift.X;
-            this.shift.Y = data.Shift.Y;
-
-            this.defaultShift.X = data.DefaultShift.X;
-            this.defaultShift.Y = data.DefaultShift.Y;
-
-            this.Zoom = data.Zoom;
         }
     }
 }
