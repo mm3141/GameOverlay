c5d4aab3572e4d75dc499252c49d8384dfdc0850
wlastas <wlastas@gmail.com>
2021-12-22 08:57:38 +0300

    Update Positioned.cs (#156)

--- GameOffsets/Objects/Components/Positioned.cs
+++ GameOffsets/Objects/Components/Positioned.cs
@@ -7,7 +7,7 @@
     {
         [FieldOffset(0x000)] public ComponentHeader Header;
 
-        [FieldOffset(0x159)] public byte Reaction;
+        [FieldOffset(0x1D9)] public byte Reaction;
         // [FieldOffset(0x1E8)] public StdTuple2D<int> GridPosition;
         // [FieldOffset(0x214)] public StdTuple2D<float> WorldPosition;
     }
