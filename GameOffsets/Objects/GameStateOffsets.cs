c6d096eb27c6636c2489e658a01a619858ca9da8
Zaafar Ahmed <zaafar.tahir@gmail.com>
2022-02-02 14:02:36 -0500

    optimized gamestate tracking to not have a unnecessary for loop.
    also, reducing number of offsets required by GH to function properly.

--- GameOffsets/Objects/GameStateOffsets.cs
+++ GameOffsets/Objects/GameStateOffsets.cs
@@ -14,15 +14,12 @@
     public struct GameStateOffset
     {
         [FieldOffset(0x08)] public StdVector CurrentStatePtr; // Used in RemoteObject -> CurrentState
-
-        //[FieldOffset(0x20)] public StdVector CurrentStatePtr; // same as CurrentStatePtr.
-        [FieldOffset(0x50)] public StdList States;
     }
 
     [StructLayout(LayoutKind.Explicit, Pack = 1)]
-    public struct StateInternalStructure
+    public struct StateHeaderStruct
     {
-        [FieldOffset(0x00)] public byte StateEnumToName;
-        [FieldOffset(0x08)] public IntPtr StatePtr;
+        [FieldOffset(0x00)] public IntPtr VirtualFunctionPtr;
+        [FieldOffset(0x0B)] public byte GameStateTypeEnum;
     }
 }
