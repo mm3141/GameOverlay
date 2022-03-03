a24448bbb7ef6a4879b1b649238b3e3c9c3a2151
Zaafar Ahmed <zaafar.tahir@gmail.com>
2022-03-03 18:39:54 -0500

    removing warnings.

--- AutoHotKeyTrigger/ProfileManager/Conditions/DynamicCondition/Interface/MonsterRarity.cs
+++ AutoHotKeyTrigger/ProfileManager/Conditions/DynamicCondition/Interface/MonsterRarity.cs
@@ -2,14 +2,40 @@
 {
     using System;
 
+    /// <summary>
+    ///     different rarity of monster available in the game.
+    /// </summary>
     [Flags]
     public enum MonsterRarity
     {
+        /// <summary>
+        ///     Monsters with red icons (i.e. white monsters).
+        /// </summary>
         Normal = 1 << 0,
+
+        /// <summary>
+        ///     Monsters with blue icons (i.e. blue monsters).
+        /// </summary>
         Magic = 1 << 1,
+
+        /// <summary>
+        ///     Monsters with yellow icons (i.e. yellow monsters).
+        /// </summary>
         Rare = 1 << 2,
+
+        /// <summary>
+        ///     Monsters with big red icon (i.e. golden monsters).
+        /// </summary>
         Unique = 1 << 3,
+
+        /// <summary>
+        ///     All rarity of monsters.
+        /// </summary>
         Any = Normal | Magic | Rare | Unique,
+
+        /// <summary>
+        ///     Rare or above.
+        /// </summary>
         AtLeastRare = Rare | Unique
     }
 }
