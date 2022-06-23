07df7857d47669221e6dc09cfb8e4fa642f2c9e9
Zaafar <5079646+zaafar@users.noreply.github.com>
2022-06-23 18:41:03 -0400

    fixes https://github.com/zaafar/GameOverlay/issues/225

--- GameOffsets/Objects/States/InGameState/ServerDataOffset.cs
+++ GameOffsets/Objects/States/InGameState/ServerDataOffset.cs
@@ -8,7 +8,7 @@ namespace GameOffsets.Objects.States.InGameState
     public struct ServerDataStructure
     {
         public const int SKIP = 0x9300; // for reducing struct size.
-        [FieldOffset(0x9378 - SKIP)] public StdVector PlayerInventories; // InventoryArrayStruct
+        [FieldOffset(0x9188 + 0x270 - SKIP)] public StdVector PlayerInventories; // InventoryArrayStruct
 
     }
 
