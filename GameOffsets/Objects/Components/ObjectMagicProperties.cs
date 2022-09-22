61bd612c68d2eda82e3472c962a1d5607418d94e
Violet Vibes <qsato@protonmail.com>
2022-09-22 18:44:41 +0200

    Update Rarity offset

--- GameOffsets/Objects/Components/ObjectMagicProperties.cs
+++ GameOffsets/Objects/Components/ObjectMagicProperties.cs
@@ -6,6 +6,6 @@
     public struct ObjectMagicPropertiesOffsets
     {
         [FieldOffset(0x00)] public ComponentHeader Header;
-        [FieldOffset(0x9C)] public int Rarity;
+        [FieldOffset(0x13C)] public int Rarity;
     }
 }
